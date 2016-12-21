eea9dd84484ae819e7958547af5789edfa58dcd4
Erh-Li Shen <erh-li.shen@workday.com>
2016-12-21 13:38:57 -0800

    Support CSR manual balance override

--- balchecker.py
+++ balchecker.py
@@ -22,7 +22,9 @@ def getBalance(cardInfo):
         initialBalance = match[2][1]
         cashbackMatch = re.findall(r"INTELISPEND - EGIFT.+\"textRightAlign\">\$([\d.]+)", response.text)
         cashback = reduce(lambda x, y: x+float(y), cashbackMatch, 0.0) if len(cashbackMatch) > 0 else 0.0
-        return {'lastFour': lastFour, 'availableBalance': availableBalance, 'initialBalance': initialBalance, 'cashback': "${0:.2f}".format(cashback)}
+        csrOverrideMatch = re.findall(r"<td>\s*<\/td><td Class=\"textRightAlign\">\$([\d.]+)", response.text)
+        csrOverride = reduce(lambda x, y: x+float(y), csrOverrideMatch, 0.0) if len(csrOverrideMatch) > 0 else 0.0
+        return {'lastFour': lastFour, 'availableBalance': availableBalance, 'initialBalance': initialBalance, 'cashback': "${0:.2f}".format(cashback), 'csrOverride': "${0:.2f}".format(csrOverride)}
 
     return {'lastFour': '-1', 'availableBalance': '0', 'initialBalance': '0', 'cashback': '0'}
 
@@ -62,8 +64,8 @@ if __name__ == "__main__":
     # execute only if run as a script
     f = open(fileName, 'r')
 
-    print '{:>13} {:>10} {:>8} {:>9}'.format('Last 4 Digits', 'Available', 'Initial', 'Cashback')
-    print '==========================================='
+    print '{:>6} {:>10} {:>8} {:>9} {:>9}'.format('Last 4', 'Available', 'Initial', 'Cashback', 'Override')
+    print '=============================================='
 
     for row in csv.reader(f):
         cardInfo = validateCard(row)
@@ -72,8 +74,8 @@ if __name__ == "__main__":
 
         balance = getBalance(cardInfo)
         if balance.get('lastFour') == '-1':
-            print '{:>13} {:>29}'.format(cardInfo.get('CardNumber')[-4:], 'Card not found')
+            print '{:>6} {:>29}'.format(cardInfo.get('CardNumber')[-4:], 'Card not found')
         else:
-            print '{:>13} {:>10} {:>8} {:>9}'.format(balance.get('lastFour'), balance.get('availableBalance'), balance.get('initialBalance'), balance.get('cashback'))
+            print '{:>6} {:>10} {:>8} {:>9} {:>9}'.format(balance.get('lastFour'), balance.get('availableBalance'), balance.get('initialBalance'), balance.get('cashback'), balance.get('csrOverride'))
 
     f.close()
