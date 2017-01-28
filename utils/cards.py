caaac63b79bf8a50c74b0a0ee36b2bf68f4a5b94
Chinghan Yu <chinghanyu@ntu.edu.tw>
2017-01-27 21:54:38 -0800

    Accompany with new website design
    BHN updates their website so the reply messages become different.
    Now the messages are parsed with static methods defined in
    'VisaGiftCard' class. We still use the pattern matching trick but
    now the codes are organized in a more readable way.

---
+++ utils/cards.py
@@ -0,0 +1,169 @@
+# Visa Gift Card
+import re
+
+class VisaGiftCard:
+    """A Visa Gift Card
+
+    Input arguments:
+    cardNumber -- 16-digits card number
+    expirationMonth -- 2-digit expiration month
+    expirationYear -- 2-digit expiration year
+    cvv -- 3-digit security code
+    postal -- 5-digit zip code
+    initBalance -- initial load value
+    currBalance -- current remaining value
+    transactions -- list of transactions
+
+    """
+
+    def __init__(self, cardNumber, expirationMonth, expirationYear, cvv, **kwargs):
+        self.cardNumber = cardNumber
+        self.expirationMonth = expirationMonth
+        self.expirationYear = expirationYear
+        self.cvv = cvv
+        self.postal = kwargs.get('postal', '00000');
+        self.initBalance = kwargs.get('initBalance', 0)
+        self.currBalance = kwargs.get('currBalance', 0)
+        self.transactions = kwargs.get('transactions', list());
+
+    @staticmethod
+    def parse(msg):
+        """Given the response message, parse it and return a VisaGiftCard object."""
+
+    @staticmethod
+    def parseLastFour(msg):
+        """Given the response message, extract the last four digits of the card.
+
+        Example message:
+            ...
+            <div class="data-box">
+                <div class="name">Card Ending</div>
+                <div class="value">7117</div>
+            </div>
+            ...
+        """
+        pattern = ("<div class=\"data-box\">[\s]*"
+                   "<div class=\"name\">Card Ending</div>[\s]*"
+                   "<div class=\"value\">(\d\d\d\d)</div>[\s]*"
+                   "</div>")
+        match = re.findall(pattern, msg)
+
+        if len(match) == 1:
+            return match[0]
+
+        if len(match) > 1:
+            return 'Error'
+
+        return 'N/A'
+
+    @staticmethod
+    def parseInitBalance(msg):
+        """Given the response message, extract the initial balance of the card.
+
+        Example message:
+            ...
+            <div class="data-box">
+                <div class="name">Available Balance</div>
+                <div class="value">$50.00</div>
+            </div>
+            ...
+        """
+        pattern = ("<div class=\"data-box\">[\s]*"
+                   "<div class=\"name\">Initial Balance</div>[\s]*"
+                   "<div class=\"value\">\$(\d+.\d\d)</div>[\s]*"
+                   "</div>")
+        match = re.findall(pattern, msg)
+
+        if len(match) == 1:
+            return match[0]
+
+        if len(match) > 1:
+            return 'Error'
+
+        return 'N/A'
+
+    @staticmethod
+    def parseCurrBalance(msg):
+        """Given the response message, extract the current balance of the card.
+
+        Example message:
+            ...
+            <div class="data-box">
+                <div class="name">Available Balance</div>
+                <div class="value">$50.00</div>
+            </div>
+            ...
+        """
+        pattern = ("<div class=\"data-box\">[\s]*"
+                   "<div class=\"name\">Available Balance</div>[\s]*"
+                   "<div class=\"value\">\$(\d+.\d\d)</div>[\s]*"
+                   "</div>")
+        match = re.findall(pattern, msg)
+
+        if len(match) == 1:
+            return match[0]
+
+        if len(match) > 1:
+            return 'Error'
+
+        return 'N/A'
+
+    @staticmethod
+    def parseFiveBackAmount(msg):
+        """Given the response message, extract the five back amount on the card.
+
+        Example message:
+            ...
+            <div class="col-xs-5ths transaction-desc">
+                INTELISPEND - EGIFT -
+            </div>
+            <div class="col-xs-5ths transaction-amount">
+                $25.00
+            </div>
+            ...
+        """
+        pattern = ("<div class=\"col-xs-5ths transaction-desc\">[\s]*"
+                   "INTELISPEND - EGIFT[ -]*[\s]*"
+                   "</div>[\s]*"
+                   "<div class=\"col-xs-5ths transaction-amount\">[\s]*"
+                   "\$(\d+.\d\d)[\s]*"
+                   "</div>")
+        match = re.findall(pattern, msg)
+
+        if len(match) == 1:
+            return match[0]
+
+        if len(match) > 1:
+            return 'Error'
+
+        return 'N/A'
+
+    @staticmethod
+    def parseOverrideAmount(msg):
+        """Given the response message, extract the override amount, the amount
+        that overrides by customer service, on the card.
+
+        Example message:
+            ...
+            <div class="col-xs-5ths transaction-desc">
+
+            </div>
+            <div class="col-xs-5ths transaction-amount">
+                $25.00
+            </div>
+            ...
+        """
+        pattern = ("<div class=\"col-xs-5ths transaction-desc\">[\s]*"
+                   "</div>[\s]*"
+                   "<div class=\"col-xs-5ths transaction-amount\">[\s]*"
+                   "\$(\d+.\d\d)[\s]*"
+                   "</div>")
+        match = re.findall(pattern, msg)
+
+        if len(match) == 1:
+            return match[0]
+
+        if len(match) > 1:
+            return 'Error'
+
+        return "N/A"
